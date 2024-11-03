48a8e89ac2b15d8d39d74842e9222c9404e367a9
Enzanto <freddyg91@gmail.com>
2024-11-03 08:21:46 +0100

    inported system files and changes names

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, home-manager, nixpkgs, ... }@inputs:
@@ -35,16 +35,16 @@
         forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
       overlays = import ./overlays { inherit inputs; };
       nixosConfigurations = {
-        your-host = nixpkgs.lib.nixosSystem {
+        nixos-vm = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
-          modules = [ ./hosts/your-host ];
+          modules = [ ./hosts/nixos-vm ];
         };
       };
       homeConfigurations = {
-        "your-name@your-host" = home-manager.lib.homeManagerConfiguration {
+        "fredrik@nixos-vm" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages."x86_64-linux";
           extraSpecialArgs = { inherit inputs outputs; };
-          modules = [ ./home/your-name/your-host.nix ];
+          modules = [ ./home/fredrik/nixos-vm.nix ];
         };
       };
     };
