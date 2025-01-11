a9900254e7df9a243041661c1859ca7a69a045ee
Fredrik Gjellestad <freddyg91@gmail.com>
2025-01-11 11:44:29 +0100

    added user config and keybindings.  added python extension

--- home/features/desktop/vscode.nix
+++ home/features/desktop/vscode.nix
@@ -18,6 +18,179 @@ in {
                 samuelcolvin.jinjahtml
                 oderwat.indent-rainbow
                 james-yu.latex-workshop
+                ms-python.python
+                ms-python.vscode-pylance
+            ];
+            userSettings = {
+                "zenMode.hideLineNumbers" = false;
+                "zenMode.centerLayout" = false;
+                "zenMode.showTabs" = "none";
+                "editor.minimap.enabled" = false;
+                # vim settings
+                "vim.leader" = "<space>";
+                "vim.normalModeKeyBindingsNonRecursive" = [
+                    # switch between buffers
+                    { 
+                        before = ["<S-h>"];
+                        "commands" = [":bprevious"];
+                    }
+                    { 
+                        before = ["<S-l>"];
+                        "commands" = [":bnext"];
+                    }
+
+                    #splits
+                    { 
+                        before = ["leader" "v"];
+                        "commands" = [":vsplit"];
+                    }
+                    {
+                        before = ["leader" "s"];
+                        "commands" = [":split"];
+                    }
+
+                    # panes
+                    {
+                        before = ["leader"  "h"];
+                        "commands" = ["workbench.action.focusLeftGroup"];
+                    }
+                    {
+                        before = ["leader"  "j"];
+                        "commands" = ["workbench.action.focusBelowGroup"];
+                    }
+                    {
+                        before = ["leader"  "k"];
+                        "commands" = ["workbench.action.focusAboveGroup"];
+                    }
+                    {
+                        before = ["leader"  "l"];
+                        "commands" = ["workbench.action.focusRightGroup"];
+                    }
+                    # NICE TO HAVE
+                    {
+                        before = ["leader"  "return"];
+                        "commands" = ["python.execInTerminal-icon"];
+                    }
+                    {
+                        before = ["leader"  "w"];
+                        "commands" = [":w!"];
+                    }
+                        #{ before = ["leader", "q"], "commands": [":q!"] };
+                    { 
+                        before = ["leader"  "x"];
+                        "commands" = [":x!"];
+                    }
+                    {
+                        before = ["["  "d"];
+                        "commands" = ["editor.action.marker.prev"];
+                    }
+                    {
+                        before = ["]"  "d"];
+                        "commands" = ["editor.action.marker.next"];
+                    }
+                    {
+                        before = ["<leader>"  "c"  "a"];
+                        "commands" = ["editor.action.quickFix"];
+                    }
+                    { 
+                        before = ["leader"  "f"];
+                        "commands" = ["workbench.action.quickOpen"];
+                    }
+                    { 
+                        before = ["leader"  "p"];
+                        "commands" = ["editor.action.formatDocument"];
+                    }
+                    {
+                        before = ["g"  "h"];
+                        "commands" = ["editor.action.showDefinitionPreviewHover"];
+                    }
+                ];
+                "vim.visualModeKeyBindings" = [
+                    # Stay in visual mode while indenting
+                    { 
+                        before = ["<"];
+                        "commands" = ["editor.action.outdentLines"];
+                    }
+                    { 
+                        before = [">"];
+                        "commands" = ["editor.action.indentLines"];
+                    }
+                    # Move selected lines while staying in visual mode
+                    { 
+                        before = ["J"];
+                        "commands" = ["editor.action.moveLinesDownAction"];
+                    }
+                    { 
+                        before = ["K"];
+                        "commands" = ["editor.action.moveLinesUpAction"];
+                    }
+                    # toggle comment selection
+                    { 
+                        before = ["leader" "c"];
+                        "commands" = ["editor.action.commentLine"];
+                        }
+                ];
+                "window.zoomLevel" = 1.5;
+                "breadcrumbs.enabled" = false;
+                "workbench.activityBar.location" = "hidden";
+            };
+            keybindings = [
+                {
+                    key = "ctrl+shift+a";
+                    command = "workbench.action.terminal.focusNext";
+                    when = "terminalFocus";
+                }
+                {
+                    key = "ctrl+shift+b";
+                    command = "workbench.action.terminal.focusPrevious";
+                    when = "terminalFocus";
+                }
+                {
+                    key = "ctrl+shift+j";
+                    command = "workbench.action.togglePanel";
+                }
+                {
+                    key = "ctrl+shift+n";
+                    command = "workbench.action.terminal.new";
+                    when = "terminalFocus";
+                }
+                {
+                    command = "workbench.action.toggleSidebarVisibility";
+                    key = "ctrl+e";
+                }
+                {
+                    command = "workbench.files.action.focusFilesExplorer";
+                    key = "ctrl+e";
+                    when = "editorTextFocus";
+                }
+                {
+                    key = "n";
+                    command = "explorer.newFile";
+                    when = "filesExplorerFocus && !inputFocus";
+                }
+                {
+                    command = "renameFile";
+                    key = "r";
+                    when = "filesExplorerFocus && !inputFocus";
+                }
+                {
+                    key = "shift+n";
+                    command = "explorer.newFolder";
+                    when = "explorerViewletFocus";
+                }
+                {
+                    command = "deleteFile";
+                    key = "d";
+                    when = "filesExplorerFocus && !inputFocus";
+                }
+                {
+                    key = "ctrl+shift+5";
+                    command = "editor.emmet.action.matchTag";
+                }
+                {
+                    key = "ctrl+z";
+                    command = "workbench.action.toggleZenMode";
+                }
             ];
         };
     };
