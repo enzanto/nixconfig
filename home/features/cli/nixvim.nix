a5dd65925a1176623273b9d7b5c9207461320085
Fredrik Gjellestad <fredrik@nerdiverset.no>
2025-05-03 12:37:56 +0200

    changed to nixvim

--- home/features/cli/nixvim.nix
+++ home/features/cli/nixvim.nix
@@ -1,43 +1,101 @@
 {
-    config,
-    lib,
-    pkgs,
-    nixvim,
-    ...
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 with lib; let
-    cfg = config.features.cli.nixvim;
+  cfg = config.features.cli.nixvim;
 in {
-    options.features.cli.nixvim.enable = mkEnableOption "Enables nixvim";
+  options.features.cli.nixvim.enable = mkEnableOption "Enables nixvim";
 
-    config = mkIf cfg.enable {
-        programs.nixvim = {
-            enable = true;
-            defaultEditor = true;
-            viAlias = true;
-            vimAlias = true;
-            # extraConfig = ''
-            #     set number relativenumber autoindent tabstop=4 shiftwidth=4 mouse=a 
-            #     filetype plugin indent on
-            #     syntax enable
-            #     noremap <C-e> :NERDTreeToggle <CR>
-            #     let g:UltiSnipsSnippetDirectories=[$HOME.'/.config/nvim/UltiSnips']
-            #     let g:UltiSnipsExpandTrigger       = '<Tab>'   
-            #     let g:UltiSnipsJumpForwardTrigger  = '<Tab>'    
-            #     let g:UltiSnipsJumpBackwardTrigger = '<S-Tab>'
-            #   '';
-            # plugins = with pkgs.vimPlugins; [
-            #     vimtex
-            #     nerdtree
-            #     #vim-snippets
-            #     vim-airline
-            #     #ultisnips
-            #     nvim-lspconfig
-            #     nvim-cmp
-            #     cmp-nvim-lsp
-            #     cmp-buffer
-            #     gruvbox-nvim
-            # ];
+  config = mkIf cfg.enable {
+    programs.nixvim = {
+      enable = true;
+      colorschemes.gruvbox.enable = true;
+      # colorschemes.catppuccin.enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      # extraConfig = ''
+      #     set number relativenumber autoindent tabstop=4 shiftwidth=4 mouse=a
+      #     filetype plugin indent on
+      #     syntax enable
+      #     noremap <C-e> :NERDTreeToggle <CR>
+      #     let g:UltiSnipsSnippetDirectories=[$HOME.'/.config/nvim/UltiSnips']
+      #     let g:UltiSnipsExpandTrigger       = '<Tab>'
+      #     let g:UltiSnipsJumpForwardTrigger  = '<Tab>'
+      #     let g:UltiSnipsJumpBackwardTrigger = '<S-Tab>'
+      #   '';
+      globals.mapleader = " ";
+      globals.maplocalleader = " ";
+      opts = {
+        number = true;
+        relativenumber = true;
+        autoindent = true;
+        tabstop = 4;
+        shiftwidth = 4;
+        mouse = "a";
+        spell = true;
+      };
+      keymaps = [
+        {
+          action = "<cmd>:w<cr>";
+          key = "<leader>w";
+        }
+        {
+          action = ":CommentToggle<cr>";
+          key = "<leader>c";
+        }
+      ];
+      plugins = {
+        cmp = {
+          enable = true;
+          autoEnableSources = true;
+          settings.sources = [
+            {name = "nvim_lsp";}
+            {name = "path";}
+            {name = "buffer";}
+          ];
         };
+        lsp = {
+          enable = true;
+          servers = {
+            lua-ls.enable = true;
+            pylsp.enable = true;
+            nixd.enable = true;
+            texlab.enable = true;
+          };
+        };
+        lualine.enable = true;
+		treesitter = {
+		enable = true;
+		folding = true;
+		# settings.indent = true;
+		settings.highlight = {
+			enable = true;
+		};
+		};
+		# treesitter.enable = true;
+		# treesitter.folding = true;
+		treesitter-textobjects.enable = true;
+        vimtex.enable = true;
+      };
+      extraPlugins = with pkgs.vimPlugins; [
+        {
+          plugin = nvim-comment;
+          config = ''lua require("nvim_comment").setup()'';
+        }
+		{
+			plugin = pkgs.fetchFromGitHub {
+				owner = "wookayin";
+				repo = "semshi";
+				rev = "v0.3.0";
+				sha256 = "sha256-gQy8rQBTzODp8OfnHdggxSq275/9T8feJAkWzH+CdvU=";
+			};
+		}
+        UltiSnips
+      ];
     };
+  };
 }
