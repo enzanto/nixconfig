1b4992cf38989c79aff591f8e0d26acc81a37f66
Fredrik Gjellestad <fredrik@nerdiverset.no>
2025-07-12 14:38:00 +0200

    Added workspaces to laptop

--- home/features/desktop/hyprConfig/razorcrest.nix
+++ home/features/desktop/hyprConfig/razorcrest.nix
@@ -3,6 +3,17 @@
     "eDP-1, 1920x1080,0x0,1"
   ];
 
+  workspace = [
+    "1, monitor:eDP-1"
+    "2, monitor:eDP-1"
+    "3, monitor:eDP-1"
+    "4, monitor:eDP-1"
+    "5, monitor:eDP-1"
+    "6, monitor:eDP-1"
+    "7, monitor:eDP-1"
+    "8, monitor:eDP-1"
+  ];
+
   hyprpaper = {
     enable = true;
     settings = {
@@ -10,11 +21,11 @@
       splash = false;
       splash_offset = 2.0;
 
-      preload =
-          [ "/home/fredrik/Pictures/wallpapers/anime_girl_alone_5k-1366x768.jpg" ];
+      preload = ["/home/fredrik/Pictures/wallpapers/anime_girl_alone_5k-1366x768.jpg"];
 
       wallpaper = [
-          "eDP-1,/home/fredrik/Pictures/wallpapers/anime_girl_alone_5k-1366x768.jpg"];
+        "eDP-1,/home/fredrik/Pictures/wallpapers/anime_girl_alone_5k-1366x768.jpg"
+      ];
     };
   };
 }
