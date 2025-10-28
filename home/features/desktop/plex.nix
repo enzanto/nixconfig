be532df5c11e3c570441a6f6f5cb002d2c956432
Fredrik Gjellestad <fredrik@nerdiverset.no>
2025-10-28 18:49:50 +0100

    fixed new desktop launcher for plex.

--- home/features/desktop/plex.nix
+++ home/features/desktop/plex.nix
@@ -1,26 +1,35 @@
 {
-    config,
-    lib,
-    pkgs,
-    ...
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 with lib; let
-    cfg = config.features.desktop.plex;
+  cfg = config.features.desktop.plex;
 in {
-    options.features.desktop.plex.enable = mkEnableOption "enable plex";
+  options.features.desktop.plex.enable = mkEnableOption "enable plex";
 
-    config = mkIf cfg.enable {
-        home.packages = with pkgs; [
-            plex-desktop
-        ];
-        xdg.portal.enable = true;
-        xdg.portal.xdgOpenUsePortal = true;
-        xdg.portal.configPackages = [
-            pkgs.gnome-session
-        ];
-        xdg.portal.extraPortals = [
-            pkgs.kdePackages.xdg-desktop-portal-kde
-            pkgs.xdg-desktop-portal-gtk
-        ];
-        };
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      plex-desktop
+    ];
+    xdg.portal.enable = true;
+    xdg.portal.xdgOpenUsePortal = true;
+    xdg.portal.configPackages = [
+      pkgs.gnome-session
+    ];
+    xdg.portal.extraPortals = [
+      pkgs.kdePackages.xdg-desktop-portal-kde
+      pkgs.xdg-desktop-portal-gtk
+    ];
+
+    xdg.desktopEntries.plex-desktop = {
+      name = "Plex media player";
+      exec = "env QT_QPA_PLATFORM=xcb plex-desktop";
+      icon = "plex"; # Adjust if needed
+      type = "Application";
+      categories = ["AudioVideo"];
+      terminal = false;
+    };
+  };
 }
