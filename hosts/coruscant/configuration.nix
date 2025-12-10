63fc38e06b6a534a208ceb7b4ae90ecbc50095aa
Fredrik Gjellestad <fredrik@nerdiverset.no>
2025-12-10 17:40:23 +0100

    Bought a printer

--- hosts/coruscant/configuration.nix
+++ hosts/coruscant/configuration.nix
@@ -114,7 +114,7 @@
   console.keyMap = "no";
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
