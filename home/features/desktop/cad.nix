4c8ead65f6723d79cbbe246fca0f8740aaa8febc
Fredrik Gjellestad <fredrik@nerdiverset.no>
2026-04-28 13:48:08 +0200

    added freecad workaround

--- home/features/desktop/cad.nix
+++ home/features/desktop/cad.nix
@@ -13,7 +13,20 @@ in {
     home.packages = with pkgs; [
       bambu-studio
       blender
-      freecad
+      # freecad
+      (pkgs.writeShellScriptBin "freecad" ''
+        export QT_QPA_PLATFORM=xcb
+        exec ${pkgs.freecad}/bin/freecad "$@"
+      '')
     ];
+    xdg.desktopEntries.freecad = {
+      name = "FreeCAD";
+      genericName = "3D CAD Modeler";
+      comment = "Parametric 3D CAD modeler";
+      exec = "freecad %F";
+      icon = "${pkgs.freecad}/share/icons/hicolor/256x256/apps/freecad.png";
+      terminal = false;
+      categories = ["Engineering"];
+    };
   };
 }
